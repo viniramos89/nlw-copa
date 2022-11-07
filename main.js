9983c362c6f646417885f2f2e367bf46df9f74c4
Vinicius Ramos <viniramos89@gmail.com>
2022-11-06 21:40:13 -0300

    :tada: initial commit

---
+++ main.js
@@ -0,0 +1,43 @@
+function createGame(player1, hour, player2) {
+  return `
+  <li>
+  <img src="./assets/icon-${player1}.svg" alt="Seleção do ${player1}">
+  <strong>${hour}</strong>
+  <img src="./assets/icon-${player2}.svg" alt="Seleção da ${player2}">
+  </li>
+  `
+}
+
+let delay = 0;
+function createCard(date, day, games) {
+  delay = delay + 0.3;
+  return `
+  <div class="card" style="animation-delay: ${delay}s">
+          <h2>${date} <span>${day}</span></h2>
+          <ul>
+            ${games}
+          </ul>
+        </div>
+  `
+}
+
+document.querySelector("#cards").innerHTML = 
+      
+        createCard(
+          "24/11",
+          "quinta",
+          createGame("uruguay", "10:00", "south-korea") +
+            createGame("brazil", "13:00", "serbia"))+
+
+        createCard(
+          "28/11",
+          "segunda",
+          createGame("brazil", "13:00", "suiça") +
+            createGame("portugal", "16:00", "uruguay"))+
+
+       createCard(
+         "02/12",
+         "sexta",
+         createGame("ghana", "12:00", "uruguay") +
+           createGame("cameroon", "16:00", "brazil"))
+      
